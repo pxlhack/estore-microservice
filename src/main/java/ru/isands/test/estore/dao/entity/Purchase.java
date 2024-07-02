e4b97fb42ff07b044c6bfeecaed3e74d67e28872
pxlhack <nellsouhd@gmail.com>
2024-07-02 13:57:51 +0700

    added field purchaseType in class Purchase

--- src/main/java/ru/isands/test/estore/dao/entity/Purchase.java
+++ src/main/java/ru/isands/test/estore/dao/entity/Purchase.java
@@ -1,19 +1,12 @@
 package ru.isands.test.estore.dao.entity;
 
 
-import java.io.Serializable;
-import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.TableGenerator;
-
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -21,45 +14,46 @@ import lombok.Setter;
 @Table(name = "store_purchase")
 public class Purchase implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	
-	/**
-	 * Идентификатор покупки
-	 */
-	@Id
-	@GeneratedValue(strategy = GenerationType.TABLE, generator = "purchase_counter")
-	@TableGenerator(name = "purchase_counter", pkColumnName = "name", pkColumnValue = "ru.isands.test.estore.dao.entity.Purchase", table = "counter", valueColumnName = "currentid", allocationSize = 1)
-	@Column(name = "id_", unique = true, nullable = false)
-	Long id;
-	
-	/**
-	 * Идентификатор товара
-	 */
-	@Column(name = "electroId", nullable = false)
-	Long electroId;
-	
-	/**
-	 * Идентификатор сотрудника
-	 */
-	@Column(name = "employeeId", nullable = false)
-	Long employeeId;
-	
-	/**
-	 * Идентификатор магазина
-	 */
-	@Column(name = "shopId", nullable = false)
-	Long shopId;
-	
-	/**
-	 * Дата совершения покупки
-	 */
-	@Column(name = "purchaseDate", nullable = false)
-	Date purchaseDate;
-	
-	/**
-	 * Способ оплаты
-	 */
-	@Column(name = "type", nullable = false)
-	int type;
-	
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Идентификатор покупки
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "purchase_counter")
+    @TableGenerator(name = "purchase_counter", pkColumnName = "name", pkColumnValue = "ru.isands.test.estore.dao.entity.Purchase", table = "counter", valueColumnName = "currentid", allocationSize = 1)
+    @Column(name = "id_", unique = true, nullable = false)
+    Long id;
+
+    /**
+     * Идентификатор товара
+     */
+    @Column(name = "electroId", nullable = false)
+    Long electroId;
+
+    /**
+     * Идентификатор сотрудника
+     */
+    @Column(name = "employeeId", nullable = false)
+    Long employeeId;
+
+    /**
+     * Идентификатор магазина
+     */
+    @Column(name = "shopId", nullable = false)
+    Long shopId;
+
+    /**
+     * Дата совершения покупки
+     */
+    @Column(name = "purchaseDate", nullable = false)
+    Date purchaseDate;
+
+    /**
+     * Тип покупки
+     */
+    @ManyToOne
+    @JoinColumn(name = "purchase_type_id", nullable = false)
+    private PurchaseType purchaseType;
+
 }
