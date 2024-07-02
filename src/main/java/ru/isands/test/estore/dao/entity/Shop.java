fe7af96c87ab8876f090b777a8612f649394c29c
pxlhack <nellsouhd@gmail.com>
2024-07-02 13:10:37 +0700

    added class Shop

---
+++ src/main/java/ru/isands/test/estore/dao/entity/Shop.java
@@ -0,0 +1,33 @@
+package ru.isands.test.estore.dao.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "shop")
+public class Shop {
+
+    /**
+     * Идентификатор магазина
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    Long id;
+
+    /**
+     * Наименование магазина
+     */
+    @Column(length = 150, nullable = false)
+    private String name;
+
+    /**
+     * Адрес магазина
+     */
+    @Column(nullable = false)
+    private String address;
+}
