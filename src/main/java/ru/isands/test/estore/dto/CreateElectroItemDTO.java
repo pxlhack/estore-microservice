dac7b9dbd987c899420934c0058adf2c5934250e
pxlhack <nellsouhd@gmail.com>
2024-07-02 18:45:18 +0700

    added class CreateElectroItemDTO

---
+++ src/main/java/ru/isands/test/estore/dto/CreateElectroItemDTO.java
@@ -0,0 +1,12 @@
+package ru.isands.test.estore.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateElectroItemDTO {
+    private String name;
+    private Long electroTypeId;
+    private Long price;
+    private int count;
+    private String description;
+}
