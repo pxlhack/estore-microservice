ce119368ae532d6c547ad297e81b14f3f1d4afca
pxlhack <nellsouhd@gmail.com>
2024-07-02 13:03:47 +0700

    added class PurchaseType

---
+++ src/main/java/ru/isands/test/estore/dao/entity/PurchaseType.java
@@ -0,0 +1,27 @@
+package ru.isands.test.estore.dao.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "purchase_type")
+public class PurchaseType {
+
+    /**
+     * Идентификатор типа покупки
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    Long id;
+
+    /**
+     * Наименование типа покупки
+     */
+    @Column(length = 150, nullable = false)
+    private String name;
+}
