ef2f82c5d0904932a30f1c21649e03bc581198e3
pxlhack <nellsouhd@gmail.com>
2024-07-02 14:38:02 +0700

    fixed class EmployeeRepository

--- src/main/java/ru/isands/test/estore/dao/repo/EmployeeRepository.java
+++ src/main/java/ru/isands/test/estore/dao/repo/EmployeeRepository.java
@@ -13,7 +13,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 	@Query("select e from Employee e where e.lastName = ?1 and e.firstName = ?2 and e.patronymic = ?3 and e.birthDate = ?4")
 	public Employee findFullName(String lastName, String firstName, String patronymic, Date birthDate);
 	
-	@Query("select e from Employee e where e.positionId = ?1")
-	public List<Employee> findByPosition(Long positionId);
+	List<Employee> findByPositionTypeId(Long positionId);
 	
 }
