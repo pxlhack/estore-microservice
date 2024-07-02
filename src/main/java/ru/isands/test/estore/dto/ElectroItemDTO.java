bbe16e4fb73510cef31b6ea5bb66a362e3d2e0c9
pxlhack <nellsouhd@gmail.com>
2024-07-02 19:16:12 +0700

    added class ElectroItemDTO

---
+++ src/main/java/ru/isands/test/estore/dto/ElectroItemDTO.java
@@ -0,0 +1,14 @@
+package ru.isands.test.estore.dto;
+
+import lombok.Data;
+
+@Data
+public class ElectroItemDTO {
+    private Long id;
+    private String name;
+    private Long electroTypeId;
+    private Long price;
+    private int count;
+    private boolean archive;
+    private String description;
+}
