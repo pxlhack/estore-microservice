10ed303827ce13d8d97695b121f81b173be50991
pxlhack <nellsouhd@gmail.com>
2024-07-03 15:30:11 +0700

    added methods create, getAll in class EmployeeController

--- src/main/java/ru/isands/test/estore/rest/EmployeeController.java
+++ src/main/java/ru/isands/test/estore/rest/EmployeeController.java
@@ -1,24 +1,34 @@
 package ru.isands.test.estore.rest;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.isands.test.estore.dto.CreateEmployeeDTO;
+import ru.isands.test.estore.dto.EmployeeDTO;
+import ru.isands.test.estore.service.EmployeeService;
+
+import java.util.List;
 
 @RestController
 @Tag(name = "Employee", description = "Сервис для выполнения операций над сотрудниками магазина")
 @RequestMapping("/estore/api/employee")
+@RequiredArgsConstructor
 public class EmployeeController {
-	
-	@GetMapping("/test")
-	@Operation(summary = "Тестовый метод", responses = {
-		@ApiResponse(description = "Тестовая фраза")
-	})
-	public ResponseEntity<String> test() {
-		return ResponseEntity.ok("test");
-	}
+
+    private final EmployeeService employeeService;
+
+    @GetMapping
+    @Operation(summary = "Получение списка сотрудников")
+    public ResponseEntity<List<EmployeeDTO>> getAll() {
+        return ResponseEntity.ok(employeeService.getAll());
+    }
+
+    @PostMapping
+    @Operation(summary = "Создание сотрудника")
+    public ResponseEntity<EmployeeDTO> create(@RequestBody CreateEmployeeDTO createEmployeeDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.create(createEmployeeDTO));
+    }
 }
