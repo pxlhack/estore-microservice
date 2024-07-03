604b06545eb058bed01ff4239b393adafa29b1d6
pxlhack <nellsouhd@gmail.com>
2024-07-03 17:03:59 +0700

    added class PurchaseDTO

---
+++ src/main/java/ru/isands/test/estore/dto/PurchaseDTO.java
@@ -0,0 +1,13 @@
+package ru.isands.test.estore.dto;
+
+import lombok.Data;
+
+@Data
+public class PurchaseDTO {
+    private Long id;
+    private Long electroItemId;
+    private Long employeeId;
+    private Long shopId;
+    private String purchaseDate;
+    private Long purchaseTypeId;
+}
