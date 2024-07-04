b31de6b5fc9f849f304e08f978ff32b3c4c21514
pxlhack <nellsouhd@gmail.com>
2024-07-04 12:04:06 +0700

    added method findAllWithShopAndPositionType in class EmployeeRepository

--- src/main/java/ru/isands/test/estore/dao/repo/EmployeeRepository.java
+++ src/main/java/ru/isands/test/estore/dao/repo/EmployeeRepository.java
@@ -6,13 +6,20 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import org.springframework.stereotype.Repository;
 import ru.isands.test.estore.dao.entity.Employee;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 	
 	@Query("select e from Employee e where e.lastName = ?1 and e.firstName = ?2 and e.patronymic = ?3 and e.birthDate = ?4")
 	public Employee findFullName(String lastName, String firstName, String patronymic, Date birthDate);
 	
 	List<Employee> findByPositionTypeId(Long positionId);
+
+	@Query("SELECT e FROM Employee e " +
+		   "JOIN FETCH e.shop " +
+		   "JOIN FETCH e.positionType")
+	List<Employee> findAllWithShopAndPositionType();
 	
 }
