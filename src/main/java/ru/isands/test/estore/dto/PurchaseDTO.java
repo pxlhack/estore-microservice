2444cf24940c074948fcd405ec4232622504e6b0
pxlhack <nellsouhd@gmail.com>
2024-07-04 17:24:08 +0700

    added fields electroItem, shop, purchaseType in class PurchaseDTO

--- src/main/java/ru/isands/test/estore/dto/PurchaseDTO.java
+++ src/main/java/ru/isands/test/estore/dto/PurchaseDTO.java
@@ -10,4 +10,8 @@ public class PurchaseDTO {
     private Long shopId;
     private String purchaseDate;
     private Long purchaseTypeId;
+
+    private ElectroItemDTO electroItem;
+    private ShopDTO shop;
+    private PurchaseTypeDTO purchaseType;
 }
