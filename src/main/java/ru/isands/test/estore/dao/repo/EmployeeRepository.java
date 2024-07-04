1562e74e6d48f83f98aa8c11185d11bba60f2051
pxlhack <nellsouhd@gmail.com>
2024-07-04 12:47:07 +0700

    added method findTopEmployeesByPositionAndSalesCount in class EmployeeRepository

--- src/main/java/ru/isands/test/estore/dao/repo/EmployeeRepository.java
+++ src/main/java/ru/isands/test/estore/dao/repo/EmployeeRepository.java
@@ -21,5 +21,26 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 		   "JOIN FETCH e.shop " +
 		   "JOIN FETCH e.positionType")
 	List<Employee> findAllWithShopAndPositionType();
+
+	@Query(value =
+			"SELECT " +
+			"    pt.name as position, " +
+			"    e.id as employee_id, " +
+			"    e.lastname, " +
+			"    e.firstname, " +
+			"    COUNT(p.id) as sales_count " +
+			"FROM " +
+			"    store_purchase p " +
+			"    JOIN public.store_employee e ON p.employee_id = e.id " +
+			"    JOIN public.position_type pt ON pt.id = e.position_id " +
+			"    JOIN public.electro_item ei ON p.electro_item_id = ei.id " +
+			"WHERE " +
+			"    p.purchase_date >= CURRENT_DATE - interval '1 year' " +
+			"GROUP BY " +
+			"    pt.id, e.id " +
+			"ORDER BY " +
+			"    pt.name, sales_count DESC",
+			nativeQuery = true)
+	List<Object[]> findTopEmployeesByPositionAndSalesCount();
 	
 }
