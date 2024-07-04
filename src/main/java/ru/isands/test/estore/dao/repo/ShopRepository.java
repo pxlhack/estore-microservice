f768a6a0054865f04977ca6cf585b49b0d3f6b9e
pxlhack <nellsouhd@gmail.com>
2024-07-04 15:50:27 +0700

    added method getTotalPriceByPurchaseTypeAndShopId in class ShopRepository

--- src/main/java/ru/isands/test/estore/dao/repo/ShopRepository.java
+++ src/main/java/ru/isands/test/estore/dao/repo/ShopRepository.java
@@ -1,7 +1,18 @@
 package ru.isands.test.estore.dao.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.isands.test.estore.dao.entity.Shop;
 
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+    @Query("SELECT COALESCE(SUM(ei.price), 0) " +
+           "FROM Shop shop " +
+           "JOIN shop.purchases sp " +
+           "JOIN sp.purchaseType pt " +
+           "JOIN sp.electroItem ei " +
+           "WHERE pt.name = :purchaseTypeName " +
+           "AND shop.id = :shopId")
+    Long getTotalPriceByPurchaseTypeAndShopId(@Param("purchaseTypeName") String purchaseTypeName, @Param("shopId") Long shopId);
 }
