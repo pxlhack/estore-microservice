22d9e7ecda5e3537e52fe277a30c4c35d347b132
pxlhack <nellsouhd@gmail.com>
2024-07-06 14:47:52 +0700

    added class BadRequestException

---
+++ src/main/java/ru/isands/test/estore/exception/BadRequestException.java
@@ -0,0 +1,11 @@
+package ru.isands.test.estore.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class BadRequestException extends ResponseStatusException {
+    public BadRequestException(String message) {
+        super(HttpStatus.BAD_REQUEST, message);
+    }
+}
--- src/main/java/ru/isands/test/estore/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/isands/test/estore/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package ru.isands.test.estore.handler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.isands.test.estore.exception.BadRequestException;
 import ru.isands.test.estore.exception.NotFoundException;
 
 @RestControllerAdvice
@@ -10,6 +11,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return new ResponseEntity<>(ex.getReason(), ex.getStatus());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequestException(BadRequestException ex) {
         return new ResponseEntity<>(ex.getReason(), ex.getStatus());
     }
 }
