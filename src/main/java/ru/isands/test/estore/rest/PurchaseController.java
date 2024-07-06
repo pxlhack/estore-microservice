e0c3e8d335dc7b5583ddeb8cd122347e6ff1967c
pxlhack <nellsouhd@gmail.com>
2024-07-06 17:06:01 +0700

    added validation for method create  in class PurchaseController

--- src/main/java/ru/isands/test/estore/rest/PurchaseController.java
+++ src/main/java/ru/isands/test/estore/rest/PurchaseController.java
@@ -5,17 +5,20 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.isands.test.estore.dto.CreatePurchaseDTO;
 import ru.isands.test.estore.dto.PurchaseDTO;
 import ru.isands.test.estore.service.PurchaseService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @Tag(name = "Purchase", description = "Сервис для выполнения операций над покупками")
 @RequestMapping("/estore/api/purchase")
 @RequiredArgsConstructor
+@Validated
 public class PurchaseController {
 
     private final PurchaseService purchaseService;
@@ -28,7 +31,7 @@ public class PurchaseController {
 
     @PostMapping
     @Operation(summary = "Создание покупки")
-    public ResponseEntity<List<PurchaseDTO>> create(@RequestBody CreatePurchaseDTO dto) {
+    public ResponseEntity<List<PurchaseDTO>> create(@Valid @RequestBody CreatePurchaseDTO dto) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(purchaseService.create(dto));
