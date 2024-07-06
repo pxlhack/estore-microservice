7ebcdc52c3d357b078cac6096a7ba0ad3317b604
pxlhack <nellsouhd@gmail.com>
2024-07-06 17:06:37 +0700

    added method handleValidationExceptions in class GlobalExceptionHandler

--- src/main/java/ru/isands/test/estore/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/isands/test/estore/handler/GlobalExceptionHandler.java
@@ -1,10 +1,16 @@
 package ru.isands.test.estore.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.isands.test.estore.exception.BadRequestException;
 import ru.isands.test.estore.exception.NotFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,5 +23,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<String> handleBadRequestException(BadRequestException ex) {
         return new ResponseEntity<>(ex.getReason(), ex.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
