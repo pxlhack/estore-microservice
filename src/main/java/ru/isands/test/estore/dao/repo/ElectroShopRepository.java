2bca126836428755452623519be87fdf62f57e5e
pxlhack <nellsouhd@gmail.com>
2024-07-06 15:14:12 +0700

    added method findByElectroItemIdAndShopId in class ElectroShopRepository

--- src/main/java/ru/isands/test/estore/dao/repo/ElectroShopRepository.java
+++ src/main/java/ru/isands/test/estore/dao/repo/ElectroShopRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.isands.test.estore.dao.entity.ElectroShop;
 import ru.isands.test.estore.dao.entity.ElectroShopPK;
 
+import java.util.Optional;
+
 public interface ElectroShopRepository extends JpaRepository<ElectroShop, ElectroShopPK> {
-	
+
+    Optional<ElectroShop> findByElectroItemIdAndShopId(Long electroItemId, Long shopId);
 }
