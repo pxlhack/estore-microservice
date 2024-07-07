ecc6a55ac48a6de550ab38ad5da801ecb69a9c4f
pxlhack <nellsouhd@gmail.com>
2024-07-07 21:19:55 +0700

    fixed ElectroItemRepository

--- src/main/java/ru/isands/test/estore/dao/repo/ElectroItemRepository.java
+++ src/main/java/ru/isands/test/estore/dao/repo/ElectroItemRepository.java
@@ -1,9 +1,9 @@
 package ru.isands.test.estore.dao.repo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import ru.isands.test.estore.dao.entity.ElectroItem;
 
 @Repository
-public interface ElectroItemRepository extends JpaRepository<ElectroItem, Long> {
+public interface ElectroItemRepository extends PagingAndSortingRepository<ElectroItem, Long> {
 }
