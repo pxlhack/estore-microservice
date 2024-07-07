9cf38e2ead0794e9f09ca34432640ad0604fefda
pxlhack <nellsouhd@gmail.com>
2024-07-07 21:24:33 +0700

    added method getElectroItemsPerPage in class ElectroItemController

--- src/main/java/ru/isands/test/estore/rest/ElectroItemController.java
+++ src/main/java/ru/isands/test/estore/rest/ElectroItemController.java
@@ -3,6 +3,7 @@ package ru.isands.test.estore.rest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +11,9 @@ import ru.isands.test.estore.dto.CreateElectroItemDTO;
 import ru.isands.test.estore.dto.ElectroItemDTO;
 import ru.isands.test.estore.service.ElectroItemService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Tag(name = "ElectroItem", description = "Сервис для выполнения операций над товарами")
@@ -34,4 +37,20 @@ public class ElectroItemController {
                 .body(electroItemService.create(createElectroItemDTO));
     }
 
+
+    @GetMapping("/page")
+    public ResponseEntity<Map<String, Object>> getElectroItemsPerPage(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        Page<ElectroItemDTO> pageElectroItems = electroItemService.getElectroItemsPerPage(page, size);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("items", pageElectroItems.getContent());
+        response.put("currentPage", pageElectroItems.getNumber());
+        response.put("totalItems", pageElectroItems.getTotalElements());
+        response.put("totalPages", pageElectroItems.getTotalPages());
+
+        return ResponseEntity.ok(response);
+    }
 }
