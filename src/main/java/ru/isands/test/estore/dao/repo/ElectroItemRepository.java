e65ce9b721aef1bf7bd36e767d0bd748f6dcdd45
pxlhack <nellsouhd@gmail.com>
2024-07-09 18:13:27 +0700

    updated class ElectroItemRepository

--- src/main/java/ru/isands/test/estore/dao/repo/ElectroItemRepository.java
+++ src/main/java/ru/isands/test/estore/dao/repo/ElectroItemRepository.java
@@ -5,5 +5,6 @@ import org.springframework.stereotype.Repository;
 import ru.isands.test.estore.dao.entity.ElectroItem;
 
 @Repository
-public interface ElectroItemRepository extends PagingAndSortingRepository<ElectroItem, Long> {
+public interface ElectroItemRepository extends PagingAndSortingRepository<ElectroItem, Long>,
+        ElectroItemWithElectroTypeRepository {
 }
